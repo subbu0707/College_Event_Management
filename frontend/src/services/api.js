8833b2411aa14e05b97e9d466f128a8522bfa90a
subbu0707 <23bq1a05j1@vvit.net>
2026-03-19 21:24:04 +0530

    deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
-const API_URL =
+const rawApiUrl =
   process.env.REACT_APP_API_URL ||
   "https://college-event-management-1-t83b.onrender.com";
+
+const API_URL = rawApiUrl.endsWith("/api")
+  ? rawApiUrl
+  : `${rawApiUrl.replace(/\/$/, "")}/api`;
 
 const api = axios.create({
   baseURL: API_URL,
